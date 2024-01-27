8427f88fcb2a9a44b51a99fcfa56c4326a844836
noddyarekar <157554074+noddyarekar@users.noreply.github.com>
2024-01-27 15:27:29 +0800

    Update creds.tf

--- 6-ansible-terraform/6.7-S3-Bucket-Using-Terraform/creds.tf
+++ 6-ansible-terraform/6.7-S3-Bucket-Using-Terraform/creds.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIA32EYJEZEDOV67MOD"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
   token = ""
   region = "us-east-1"
 }
