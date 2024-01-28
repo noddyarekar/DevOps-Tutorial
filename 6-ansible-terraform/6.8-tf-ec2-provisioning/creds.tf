f96a130e352e4564ccc15c3049320c3ca293585d
noddyarekar <157554074+noddyarekar@users.noreply.github.com>
2024-01-28 18:56:55 +0800

    Update creds.tf

--- 6-ansible-terraform/6.8-tf-ec2-provisioning/creds.tf
+++ 6-ansible-terraform/6.8-tf-ec2-provisioning/creds.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIAQ7ZCMYMXTD7Q2FOK"
-  secret_key = "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"
+  access_key = "AKIAUBISL2PWIUY5T2E7"
+  secret_key = "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"
   token = ""
   region = "us-east-1"
 }
